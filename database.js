5457ae942989dbe280e77a5a2bec509233ce175e
Wiraphong2003 <111116563+Wiraphong2003@users.noreply.github.com>
2023-04-11 22:53:47 +0700

    asd

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
 const dbConnection = mysql.createPool({
-    host     : 'localhost', // MYSQL HOST NAME
-    user     : 'root', // MYSQL USERNAME
-    password : 'root', // MYSQL PASSWORD
-    database : 'nodejs_login' // MYSQL DB NAME
+    host: 'mysql-119174-0.cloudclusters.net', // MYSQL HOST NAME
+    user: 'useM6', // MYSQL USERNAME
+    password: '1234/.,m', // MYSQL PASSWORD
+    database: 'dbM6' // MYSQL DB NAME
 }).promise();
 module.exports = dbConnection;
